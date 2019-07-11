0158b78028c8d2871509e64d3d126c1adb904ed9
Vivien Richter <vivien-richter@outlook.de>
2019-07-11 12:59:22 +0200

    Completely rewritten and dump feature added.

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+read -r -d '' about <<- INFO
 ########################################################
 #                                                      #
 #                C/C++ microcontroller                 #
-#             compile and transfer script              #
+#         compile, dump and programming script         #
 #                                                      #
-#                     Version 1.0                      #
+#                     Version 2.0                      #
 #                                                      #
 #  2019 by Vivien Richter <vivien-richter@outlook.de>  #
 #                                                      #
@@ -21,29 +22,108 @@
 #                                                      #
 ########################################################
 
+Usage: run.sh [-i] [-d] [-c] [-b] [-t]
+
+Options:
+    -i                    Shows target device details.
+    -d                    Creates dump files of all target device memories.
+    -c                    Deletes all built binary files.
+    -b                    Builds binary files from the source code.
+    -t                    Transfers built binary files to the target device.
+    -h, --help, -?        Shows this info text.
+INFO
+
 # Configuration
-sourceFile="main.cpp"
+sourceDirectory="./src"
+binaryDirectory="./bin"
+dumpDirectory="./dmp"
 targetDevice="atmega8"
-programmingDevice="avrispmkII"
+programmingDevice="usbasp"
+supportedMemoryTypes=(calibration eeprom efuse flash fuse hfuse lfuse lock signature application apptable boot prodsig usersig);
 
-# Enable shell option 'extglob'
-shopt -s extglob
+# Shows info text.
+showInfo() {
+	echo "$about"
+}
 
-# Cleaning
-echo -e "\033[1mCleaning..\033[0m"
-rm -v -f -r ./bin/!(.gitkeep|.|..)
+# Detects command line parameters.
+if [ $# -gt 0 ]; then
+    while [ "$1" != "" ]; do
+		case $1 in
+			-i )
+				showTargetDeviceDetails=true
+				;;
+			-d )
+				createDump=true
+				;;
+			-c )
+				clean=true
+				;;
+			-b )
+				build=true
+				;;
+			-t )
+				transfer=true
+				;;
+			-h | --help | -? )
+				showInfo
+				exit 0
+				;;
+			* )
+				showInfo
+				exit 1
+				;;
+		esac
+		shift
+	done
+else
+    showInfo
+	exit 1
+fi
 
-# Compiling
-echo -e "\033[1mCompiling..\033[0m"
-avr-gcc -v -Wall -Os -mmcu=$targetDevice -o ./bin/main.elf ./src/$sourceFile
+# Shows target device details.
+if [ "$showTargetDeviceDetails" = true ]; then
+	avrdude -p $targetDevice -c $programmingDevice -n -v 2>&1
+fi
 
-# Creating HEX image
-echo -e "\033[1mCreating HEX image..\033[0m"
-avr-objcopy -v -O ihex ./bin/main.elf ./bin/main.hex
+# Creates dump files.
+if [ "$createDump" = true ]; then
+	echo -e "\033[1mCreating dump files..\033[0m"
+	currentDateTime=$(date "+%Y.%m.%d_%H:%M:%S")
+	mkdir -v -p $dumpDirectory/$currentDateTime
+	targetInfo=$(avrdude -p $targetDevice -c $programmingDevice -n -v 2>&1)
+	echo "$targetInfo" > $dumpDirectory/$currentDateTime/info.txt
+	for memoryType in "${supportedMemoryTypes[@]}"; do
+		if grep -q -w "$memoryType" <<< $targetInfo; then
+			avrdude -p $targetDevice -c $programmingDevice -n -U $memoryType:r:$dumpDirectory/$currentDateTime/$memoryType.hex:i
+		fi
+	done
+fi
 
-# Transfer
-echo -e "\033[1mTransfer..\033[0m"
-avrdude -p $targetDevice -c $programmingDevice -e -U ./bin/main.hex
+# Deletes all built binary files.
+if [ "$clean" = true ]; then
+	echo -e "\033[1mCleaning..\033[0m"
+	rm -r -f -v $binaryDirectory
+fi
+
+# Builds binary files from the source code.
+if [ "$build" = true ]; then
+	# Creates destination directory.
+	mkdir -v -p $binaryDirectory
+	# Compiling.
+	echo -e "\033[1mCompiling..\033[0m"
+    avr-gcc -v -Wall -Os -mmcu=$targetDevice -o $binaryDirectory/flash.elf $sourceDirectory/*.cpp
+	# Creates HEX image.
+	echo -e "\033[1mCreating HEX image..\033[0m"
+	avr-objcopy -v -O ihex $binaryDirectory/flash.elf $binaryDirectory/flash.hex
+fi
+
+# Transfers compiled binary to the target device.
+if [ "$transfer" = true ]; then
+	echo -e "\033[1mTransferring..\033[0m"
+	avrdude -p $targetDevice -c $programmingDevice -e -U $binaryDirectory/flash.hex
+fi
 
 # Exit.
+echo -e "\033[1mDone.\033[0m"
 exit 0
